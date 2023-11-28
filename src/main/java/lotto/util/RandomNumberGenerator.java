2f6767718b88f4daaba67c5befd3533af158fe67
kcw0360 <kcw0360@naver.com>
2023-11-28 18:04:02 +0900

    feat(RandomNumberGenerator): 로또 발행을 위한 번호 생성기 작성

---
+++ src/main/java/lotto/util/RandomNumberGenerator.java
@@ -0,0 +1,15 @@
+package lotto.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class RandomNumberGenerator {
+
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final int PICK_NUMBER = 6;
+
+    public static List<Integer> generateNumbers() {
+        return Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, PICK_NUMBER);
+    }
+}
