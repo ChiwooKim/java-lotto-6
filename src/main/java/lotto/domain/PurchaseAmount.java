68250c0be33ce1b82cd326212f4bd262004c2de3
kcw0360 <kcw0360@naver.com>
2023-11-28 17:33:08 +0900

    feat(PurchaseAmount): 구입 금액 관련 객체 생성 및 검증

---
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -0,0 +1,34 @@
+package lotto.domain;
+
+import static lotto.util.ErrorMessage.*;
+
+public class PurchaseAmount {
+
+    public static final int REMAINDER = 0;
+    public static final int AMOUNT_UNIT = 1000;
+    public static final int INPUT_AMOUNT_MIN_LIMIT = 1000;
+    public static final int INPUT_AMOUNT_MAX_LIMIT = 100000;
+    private final int amount;
+
+    public PurchaseAmount(int amount) {
+        validate(amount);
+        this.amount = amount;
+    }
+
+    private void validate(int amount) {
+        validateAmountRange(amount);
+        validateAmountUnit(amount);
+    }
+
+    private void validateAmountRange(int amount) {
+        if (amount < INPUT_AMOUNT_MIN_LIMIT || amount > INPUT_AMOUNT_MAX_LIMIT) {
+            throw new IllegalArgumentException(INVALID_AMOUNT_RANGE.getMessage());
+        }
+    }
+
+    private void validateAmountUnit(int amount) {
+        if (amount % AMOUNT_UNIT > REMAINDER) {
+            throw new IllegalArgumentException(INVALID_AMOUNT_UNIT.getMessage());
+        }
+    }
+}
