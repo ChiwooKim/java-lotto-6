fc5d52731138ee0389ffb41b067c3f4754b3d04e
kcw0360 <kcw0360@naver.com>
2023-11-28 21:19:35 +0900

    feat(LottoService): 당첨 결과 찾는 로직 작성

--- src/main/java/lotto/LottoService/LottoService.java
+++ src/main/java/lotto/LottoService/LottoService.java
@@ -1,10 +1,45 @@
 package lotto.LottoService;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import lotto.domain.BonusNumber;
+import lotto.domain.Lotto;
 import lotto.domain.PurchaseHistory;
+import lotto.domain.Rank;
 
 public class LottoService {
 
-    public PurchaseHistory getPurchaseHistory(int number) {
+    public PurchaseHistory purchaseLotto(int number) {
         return new PurchaseHistory(number);
+    }
+
+    public HashMap<Rank, Integer> checkLottoNumbers(PurchaseHistory purchaseHistory,
+                                        Lotto winningNumbers, BonusNumber bonusNumber) {
+
+        List<Rank> matchResult = getRanks(purchaseHistory, winningNumbers, bonusNumber);
+        return getResult(matchResult);
+    }
+
+    private List<Rank> getRanks(PurchaseHistory purchaseHistory, Lotto winningNumbers, BonusNumber bonusNumber) {
+        List<Rank> matchResult = new ArrayList<>();
+        for (Lotto lotto : purchaseHistory.getPurchaseHistory()) {
+            int matchNumber = lotto.matchWinningNumber(winningNumbers.getNumbers());
+            boolean bonus = bonusNumber.isBonusNumber(lotto.getNumbers());
+            matchResult.add(Rank.findRank(matchNumber, bonus));
+        }
+        return matchResult;
+    }
+
+    private HashMap<Rank, Integer> getResult(List<Rank> results) {
+        HashMap<Rank, Integer> lottoResult = new HashMap<>();
+        for (Rank result : results) {
+            if (lottoResult.containsKey(result)) {
+                lottoResult.put(result, lottoResult.get(result) + 1);
+                continue;
+            }
+            lottoResult.put(result, 1);
+        }
+        return lottoResult;
     }
 }
