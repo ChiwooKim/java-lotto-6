c6049f66acb825caac7d692a9502c7315206b8ed
kcw0360 <kcw0360@naver.com>
2023-11-28 19:50:15 +0900

    feat(Lotto): 입력 값들의 개수, 범위, 중복 검증 메소드 작성

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,9 +1,16 @@
 package lotto.domain;
 
+import static lotto.util.ErrorMessage.*;
+
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
+
+    private static final int Lotto_size = 6;
+    private static final int LOTTO_MINIMUM_RANGE = 1;
+    private static final int LOTTO_MAXIMUM_RANGE = 45;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -12,8 +19,28 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        validateSize(numbers);
+        validateRange(numbers);
+        validateDuplications(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != Lotto_size) {
+            throw new IllegalArgumentException(INVALID_LOTTO_SIZE.getMessage());
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        for (Integer number : numbers) {
+            if (number < LOTTO_MINIMUM_RANGE || number > LOTTO_MAXIMUM_RANGE) {
+                throw new IllegalArgumentException(INVALID_OUT_OF_RANGE.getMessage());
+            }
+        }
+    }
+
+    private void validateDuplications(List<Integer> numbers) {
+        if (Set.copyOf(numbers).size() != Lotto_size) {
+            throw new IllegalArgumentException(DUPLICATE_NUMBER.getMessage());
         }
     }
 
