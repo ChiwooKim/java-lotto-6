c7ed3ddb577e3e8dc8330541594ff74a041af5c1
kcw0360 <kcw0360@naver.com>
2023-11-28 19:58:16 +0900

    feat(LottoController): 당첨 번호 입력 로직 작성

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,6 +1,10 @@
 package lotto.controller;
 
+import java.util.List;
+import lotto.LottoService.LottoService;
+import lotto.domain.Lotto;
 import lotto.domain.PurchaseAmount;
+import lotto.domain.PurchaseHistory;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -8,14 +12,23 @@ public class LottoController {
 
     private final InputView inputView;
     private final OutputView outputView;
+    private final LottoService lottoService;
 
     public LottoController(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
         this.outputView = outputView;
+        this.lottoService = new LottoService();
     }
 
     public void draw() {
         PurchaseAmount purchaseAmount = inputPurchaseAmount();
+
+        outputView.printNumberOfPurchase(purchaseAmount.getNumberOfPurchases());
+        PurchaseHistory purchaseHistory =
+                lottoService.purchaseLotto(purchaseAmount.getNumberOfPurchases());
+        outputView.printPurchaseHistory(purchaseHistory.getPurchaseHistory());
+
+        Lotto winningNumbers = inputWinningNumbers();
     }
 
     private PurchaseAmount inputPurchaseAmount() {
@@ -24,6 +37,18 @@ public class LottoController {
             try {
                 Integer amount = inputView.readPurchaseAmount();
                 return new PurchaseAmount(amount);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private Lotto inputWinningNumbers() {
+        outputView.printWinningNumbersInputMessage();
+        while (true) {
+            try {
+                List<Integer> winningNumbers = inputView.readWinningNumbers();
+                return new Lotto(winningNumbers);
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
